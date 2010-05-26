b4700ce09c2c87b0f3e5410b880243730aa6a196
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-05-26 20:40:48 -0300

    Add copyright notice on ui file

--- ui.py
+++ ui.py
@@ -1,3 +1,24 @@
+"""
+
+
+Dojotools - tools for your coding dojo session
+
+Copyright (C) 2009 Flávio Amieiro <amieiro.flavio@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 dated June, 1991.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+If you find any bugs or have any suggestions email: amieiro.flavio@gmail.com
+"""
 import os
 import sys
 import gtk
