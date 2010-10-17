292f4ba413ee89ad86340652f1a51751fdc4f0e0
César Frias <cagfrias@gmail.com>
2010-10-16 23:10:03 -0300

    Adiciona metodos has_monitor, create_monitor e get_commands

--- gedit/dojotools-gedit.py
+++ gedit/dojotools-gedit.py
@@ -13,19 +13,17 @@ from dojotools import Monitor, Timer
 
 class DojoToolsGeditHelper:
     def __init__(self, plugin, window):
-        print "Dojotools update for", self._window
         self._window = window
         self._plugin = plugin
 
     def deactivate(self):
-        print "Dojotools update for", self._window
         self._window = None
         self._plugin = None
 
     def update_ui(self):
         # Called whenever the window has been updated (active tab
         # changed, etc.)
-        print "Dojotools update for", self._window
+        pass
 
 class DojoToolsGedit(gedit.Plugin):
     def __init__(self):
@@ -34,21 +32,43 @@ class DojoToolsGedit(gedit.Plugin):
 
     def activate(self, window):
         timer = Timer(300)
-        ui = UserInterface(timer)
-        monitor = Monitor(
-            ui = ui,
-            directory = '/home/cesar/Dojo/codigosDojo/setembro/25/',
-            commands = ["python /home/cesar/Dojo/codigosDojo/setembro/25/teste_Jo_Ken_Po.py",],
-            patterns_file = '/home/cesar/Dojo/codigosDojo/setembro/25/.ignore',
-            commit = False,
-        )
-
+        self.ui = UserInterface(timer)
         self._instances[window] = DojoToolsGeditHelper(self, window)
-        gtk.main()
 
     def deactivate(self, window):
         self._instances[window].deactivate()
         del self._instances[window]
 
     def update_ui(self, window):
+        if not self.has_monitor() or self.commands == []:
+            self.create_monitor(window)
         self._instances[window].update_ui()
+
+    def has_monitor(self):
+        try:
+            if self.monitor:
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def create_monitor(self, window):
+        self.monitor = Monitor(
+            ui = self.ui,
+            directory = '/home/cesar/Dojo/codigosDojo/setembro/25/',
+            commands = self.get_commands(window),
+            patterns_file = '/home/cesar/Dojo/codigosDojo/setembro/25/.ignore',
+            commit = False,
+        )
+        return self.monitor
+
+    def get_commands(self, window):
+        documents = window.get_documents()
+        self.commands = list()
+        for document in documents:
+            if document.get_uri():
+                teste = document.get_uri().strip('file://')
+                self.commands = 'python /' + teste
+        return self.commands
+
