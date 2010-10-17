68e47bc139a955d248900f3c7f85ed77dc9b6da2
Bernardo Fontes <bernardoxhc@gmail.com>
2010-10-17 16:58:14 -0200

    Black Magic: isso funciona e atende a demanda, mas precisamos pensar melhor

--- arduino/__init__.py
+++ arduino/__init__.py
@@ -1,12 +1,17 @@
 import serial
 import glob
+import time
 
-def send_message(message):
+def send_message(test_fails):
     '''
     Sends a message to Arduino via serial
     '''
     arduino_usb = glob.glob('/dev/ttyUSB*')[0]
     arduino = serial.Serial(arduino_usb, 9600)
-    arduino.timeout = 0.1
-    arduino.write(message)
-    arduino.close()
+    time.sleep(1)
+
+    if test_fails:
+        arduino.write('G')
+        arduino.write('R' * 10)
+    else:
+        arduino.write('G' * 10)
