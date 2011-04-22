e46d192c8a8578576b3912b690a9798a2f7c1442
Joao Felipe Nicolaci Pimentel <joao@joao-R590.(none)>
2011-04-22 02:05:43 -0300

    Generators de javascript

---
+++ generators/javascript/#*up_dojotools*#Spec.js
@@ -0,0 +1,7 @@
+describe("#*up_dojotools*#", function() {
+
+  it("should return 1 when #*dojotools*# is called", function() {   
+    expect(#*dojotools*#()).toEqual(1); 
+  });
+
+});
---
+++ generators/javascript/run_specs.rb
@@ -0,0 +1,16 @@
+require 'rubygems'  
+require 'jazz_money'  
+  
+javascript_files = [  
+  '#*up_dojotools*#Spec.js'  
+]  
+  
+jasmine_spec_files = [  
+  '#*up_dojotools*#.js'
+]  
+  
+jazz_money = JazzMoney::Runner.new(javascript_files, jasmine_spec_files)
+jazz_money.call 
+jazz_money.results.each do |message|
+  exit 1 unless message[1].to_s.include? "resultpassedmessagesmessagePassed."
+end
