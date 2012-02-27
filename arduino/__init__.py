dea21ba0eca891446ec01619fe9fad02ff751b13
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2012-02-27 13:16:28 -0300

    possivel correção para problema do sinal

--- arduino/__init__.py
+++ arduino/__init__.py
@@ -13,9 +13,9 @@ def send_message(test_fails, last=False):
     
     arduino_usb = glob.glob('/dev/ttyUSB*')[0]
     serial.Serial.color = lambda self, l, v: self.write(color[l]+color[v]*10)
-    #serial.Serial.color = lambda self, l, v: self.write(color[v]*10)
+    serial.Serial.color = lambda self, l, v: self.write(color[v])
         
 
     arduino = serial.Serial(arduino_usb, 9600)
-    time.sleep(1)
+    #time.sleep(1)
     arduino.color(last, test_fails)
